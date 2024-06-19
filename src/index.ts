09a192ad5ce74b0cb7b45f988ed993727f615a0e
RobsonTrasel <robson.trasel@gmail.com>
2024-06-18 21:00:34 -0400

    chore: onde é feito inicialização do arquivo bootstrap e da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,5 @@
-import { Client, GatewayIntentBits, Interaction } from "discord.js";
-import { Config } from "./config/config";
-import { BattleMetricsService } from "./services/BattleMetricsService";
-import { CommandHandler } from "./handlers/CommandHandler";
+import { Bootstrap } from "./Bootstrap";
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+const bootstrap = new Bootstrap();
 
-const service = new BattleMetricsService(Config.serverId, Config.apiUrl);
-const commandHandler = new CommandHandler(service);
-
-client.once("ready", () => console.log(`Logado como: ${client.user?.tag}`));
-
-client.on("interactionCreate", async (interaction: Interaction) => {
-  console.log(interaction);
-  if (!interaction.isCommand()) return;
-  await commandHandler.handle(interaction);
-});
-
-client.on("messageCreate", async (message) => {
-  await commandHandler.handleMessage(message);
-});
-
-client.login(Config.discordToken);
+bootstrap.start();
